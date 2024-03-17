5730d10578d8c44614a70d3dd198f98d42303cc2
hilaire <hilaire@gnu.org>
2024-03-17 17:37:40 +0100

    Close #18

--- startIDE.sh
+++ startIDE.sh
@@ -14,10 +14,10 @@ ide=drgeoIDE
 VM=CuisVM.app/Contents/Linux-x86_64/squeak
 
 # Install image for DrGeo IDE
-cd $imageFolder
+cd $imagePath
 rm $ide.image $ide.changes $ide.user.* *.log
 cp $cuis.image $ide.image
 cp $cuis.changes $ide.changes
 cd -
 
-$VM $imageFolder/$ide -s drgeo/src/setupDrGeoDevelopment.st 
+$VM $imagePath/$ide -s drgeo/src/setupDrGeoDevelopment.st 
